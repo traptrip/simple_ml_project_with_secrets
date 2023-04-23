8d1d5c674703995cbe54307d66a3cc27322df751
traptrip <aletni@gmail.com>
2023-04-23 13:55:40 +0300

    add data preparation step

---
+++ data/preprocess_data.sh
@@ -0,0 +1,4 @@
+PYTHONPATH=$PWD python src/preprocess.py && \
+    tar -czvf data/humpback-whale-identification.tar.gz data/humpback-whale-identification && \
+    dvc add data/humpback-whale-identification.tar.gz && \
+    dvc push
