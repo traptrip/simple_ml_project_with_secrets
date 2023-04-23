5a4b9d6f868bc30cd07acf78d76a919c7989677e
traptrip <aletni@gmail.com>
2023-04-23 17:33:46 +0500

    fix full data download script

--- data/download_full_kaggle_data.sh
+++ data/download_full_kaggle_data.sh
@@ -1,1 +1,3 @@
-kaggle competitions download --unzip -p ./data/humpback-whale-identification -c humpback-whale-identification
+kaggle competitions download -p ./data/ -c humpback-whale-identification && \
+    mkdir data/humpback-whale-identification/ && \
+    unzip data/humpback-whale-identification.zip -d data/humpback-whale-identification
