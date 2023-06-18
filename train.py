f5bb97a3d6ef35c3b1c558a97f67db3bb759e387
traptrip <aletni@gmail.com>
2023-06-18 20:07:21 +0500

    remove lightning

--- train.py
+++ train.py
@@ -3,22 +3,21 @@ from pathlib import Path
 from collections import defaultdict
 
 import torch
-import pytorch_lightning as pl
-from lightning.pytorch.loggers import TensorBoardLogger
 from tqdm import tqdm
 
 from src.dataset import get_dataloaders
-from src.utils import read_config, get_last_logs_dir
-from src.net import Net
+from src.utils import read_config
+from src.training import Trainer
+from database.mongo import MongoDB
 
 
-def init_embeddings_db(train_dl, net, emb_size, batch_size, device, save_path):
+def init_embeddings_db(train_dl, net, emb_size, batch_size, device, db_client: MongoDB):
     net.to(device)
     net.eval()
     with torch.no_grad():
         embeddings = torch.zeros((len(train_dl.dataset), emb_size))
         labels = []
-        for i, (batch, label) in enumerate(tqdm(train_dl)):
+        for i, (batch, label) in enumerate(tqdm(train_dl, desc="Init embeddings DB")):
             labels.extend(label.tolist())
             embeddings[i : i + batch_size] = net(batch.to(device)).cpu()
 
@@ -34,35 +33,31 @@ def init_embeddings_db(train_dl, net, emb_size, batch_size, device, save_path):
                 torch.mean(torch.stack(embs), dim=0, keepdims=True)
             )
 
-    save_path = get_last_logs_dir(Path(save_path)) / "emb_db.pth"
-    torch.save(emb_db, save_path)
+    db_client.insert_embeddings(emb_db)
 
 
 if __name__ == "__main__":
     cfg = read_config(Path(__file__).parent / "config.yml")
 
+    # prepare database
+    logging.info("Initialize database client")
+    db_client = MongoDB(cfg.database.name, cfg.database.host, cfg.database.port)
+
     # prepare data
     logging.info("Prepare dataloaders")
     train_dl, val_dl, _ = get_dataloaders(Path(cfg.data.data_dir), cfg.train.batch_size)
 
-    # prepare trainer
-    logging.info("Prepare trainer")
-    net = Net(cfg.train)
-
-    logger = TensorBoardLogger(cfg.train.trainer.default_root_dir)
-    trainer = pl.Trainer(logger=logger, **cfg.train.trainer)
-
-    # train
     logging.info("Training")
-    trainer.fit(net, train_dl, val_dl)
+    trainer = Trainer(cfg.train)
+    trainer.train(train_dl, val_dl)
 
     # prepare embeddings_file
-    logging.info("Initialize embeddings DB")
+    logging.info("Insert embeddings into DB")
     init_embeddings_db(
         train_dl,
-        net,
+        trainer.net,
         cfg.train.net.embedding_size,
         cfg.train.batch_size,
         cfg.train.device,
-        cfg.train.save_emb_db_path,
+        db_client,
     )
